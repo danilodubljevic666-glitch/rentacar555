1946b30a7dd3ec34c863556225e04765593e07af
danilodubljevic666-glitch <danilo.dubljevic666@gmail.com>
2026-03-14 16:29:33 +0100

    fix bropj 22

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const corsOptions = {
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://rentacar555-jwgs.vercel.app/'
+        'https://rentacar555-jwgs.vercel.app'
     ],
     credentials: true,
     optionsSuccessStatus: 200
